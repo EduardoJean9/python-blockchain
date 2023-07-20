26eba8c51c5d29dfc4bb42bd886725f6ad8a2d8a
EduardoJean <Eduardojean9@gmail.com>
2023-07-20 13:50:38 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+**Activate the virtual environment**
+```
+source blockchain-env/bin/activate
+```
+
+***Install all packages ***
+```
+pip3 install -r requirements.txt
+```
+
+***Run the Tests ***
+```
+python3 -m pytest backend/tests
+```
---
+++ backend/blockchain/blockchain.py
@@ -0,0 +1,16 @@
+from backend.blockchain.block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [Block.genesis()]
+    def add_block(self, data):
+        last_block = self.chain[-1]
+        self.chain.append(Block.mine_block(self.chain[-1], data))
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+def main(): 
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+    print(blockchain)
