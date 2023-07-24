d7d9f2a793cae954190bcb0a98dcb445efc5c048
EduardoJean <Eduardojean9@gmail.com>
2023-07-23 23:53:50 -0400

    add difficulty adjust

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,6 @@
 import time
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -36,18 +37,32 @@ class Block:
         the leading 0's proof of work environment"""
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
     
     @staticmethod
     def genesis():
         return Block(**GENESIS_DATA)
+    
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE
+        increase the difficulty for quickly mined blocks
+        decrease the difficulty for slowly mined blocks
+        """
+        if(new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+        if(last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+        return 1
 
 def main():
     genesis_block = Block.genesis()
